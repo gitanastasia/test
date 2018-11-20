bbf09573ab6849a1362de8ddddad161003b720b4
anastasiya <anastasiya2211@yahoo.com>
2018-11-20 17:04:52 +0300

    Create creatName.js
    creat abbr js file

---
+++ creatName.js
@@ -0,0 +1,36 @@
+
+
+
+function init() {
+       
+    var showButton = document.getElementById('showBtn');
+    showButton.onclick = creatWord;
+    
+
+ function creatWord() {
+    var parts1 = ['c','i','u','c','p','a','q','s','e','d','i','r','p','r','l'];  
+    var parts2 = ['c','i','u','c','p','a','q','s','e','d','i','r','p','r','l'];
+    var parts3 = ['c','i','u','c','p','a','q','s','e','d','i','r','p','r','l'];
+    
+        
+    //var rand1 = Math.floor(Math.random() * parts1.length);
+    //var rand2 = Math.floor(Math.random() * parts2.length);
+    //var rand3 = Math.floor(Math.random() * parts3.length);
+    
+    //var word = parts1[rand1] + parts2[rand2] + parts3[rand3];
+    
+    //Заменили предыдующий код
+    var pickRandomPart = function(parts) {
+      return parts[Math.floor(Math.random() * parts.length)];  
+    };
+    
+    var randomWord = pickRandomPart(parts1) + pickRandomPart(parts2) + pickRandomPart(parts3);
+    //return randomWord;
+    
+    alert("Вот оно! => " + randomWord);
+    
+  }
+};
+window.onload = init;
+
+
